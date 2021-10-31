938471c42f383a273288a2e0512a3ef2e7ca0b2a
samFabrice <dsamfabrice5@gmail.com>
2021-10-31 16:36:58 +0100

    I rewrite the Vect3D class to build row vectors with variable column

--- Vect3D.cpp
+++ Vect3D.cpp
@@ -4,58 +4,37 @@
 
 using namespace std;
 
-Vect3D::Vect3D(size_t ligne, size_t colonne, double valeur):m_ligne(ligne), 
-m_colonne(colonne), m_vect3d(ligne * colonne, valeur)
+Vect3D::Vect3D(size_t colonne,double valeur):m_ligne(1), m_colonne(colonne),m_vect3d( m_ligne * m_colonne, valeur)
 {
-    if (m_colonne == m_ligne){
-        perror("Les valeurs données ne permettent pas d'avoir une matrice vecteur\n");
-        exit(-1);
+    
+}
+
+double & Vect3D::operator()(std::size_t i)
+{
+
+    return m_vect3d[i];
+}
+
+double const& Vect3D::operator()(std::size_t i) const
+{
+
+    return m_vect3d[i];
+}
+
+
+std::ostream& operator<<(std::ostream & flux, Vect3D  const & vect) {
+    size_t i ;
+    flux << "[ ";
+    for(i = 0; i < vect.nb_colonnes(); i++){
+        
+        flux << vect(i) << " ";
     }
-}
-
-
-std::size_t Vect3D::offset(std::size_t ligne, std::size_t colonne) const 
-{
-    // retourne l'indice de la matrice ou on aimerait accéder
-    assert(ligne < m_ligne && "Ligne demandée invalide.");
-    assert(colonne < m_colonne && "Colonne demandée invalide.");
-    return colonne * m_ligne + ligne;
-}
-
-
-double & Vect3D::operator()(std::size_t x, std::size_t y)
-{
-
-    return m_vect3d[offset(x, y)];
-}
-
-double const& Vect3D::operator()(std::size_t x, std::size_t y) const
-{
-
-    return m_vect3d[offset(x, y)];
-}
-
-
-size_t Vect3D::nb_lignes() const 
-{
-    return m_ligne;
-}
-
-size_t Vect3D::nb_colonnes() const 
-{
-    return m_colonne;
-}
-
-std::ostream& operator<<(std::ostream & flux, Vect3D  const & vect){
-    size_t i ; size_t j ;
-    for(i = 0; i < vect.nb_lignes(); i++){
-        for(j = 0; j < vect.nb_colonnes(); j++){
-            flux << vect(i, j) << " ";
-        }
-        flux << " \n";
-    }
-
+    flux << "] \n ";
     return flux;
 }
+
+size_t  Vect3D::nb_colonnes() const{
+    return m_colonne;
+} 
 
 
--- Vect3D.hpp
+++ Vect3D.hpp
@@ -9,15 +9,13 @@
 class Vect3D
 {
     public:
-        Vect3D(std::size_t ligne, std::size_t colonne, double valeur = 0);
-        double & operator()(std::size_t i, std::size_t j); // Accès en écriture
-        double const & operator()(std::size_t i, std::size_t j) const; // Accès en lecture
-        std::size_t nb_lignes() const ;
+        Vect3D(size_t colonne, double valeur = 0);
+        double & operator()(std::size_t i); // Accès en écriture
+        double const & operator()(std::size_t i) const; // Accès en lecture
+        Vect3D transpose();
         std::size_t nb_colonnes() const ;
 
-
     private:
-        std::size_t offset(std::size_t ligne, std::size_t colonne) const ;
         std::size_t m_ligne;
         std::size_t m_colonne;
         std::vector<double> m_vect3d;
@@ -25,6 +23,7 @@ class Vect3D
 
 };
 
-std::ostream& operator<<(std::ostream & flux, Vect3D const& vect);
+std::ostream& operator<<(std::ostream & flux, Vect3D const& vect) ;
+
 
 #endif
