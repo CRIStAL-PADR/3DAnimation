b834e96e6643da3419a698cbe92f3f117f8132f4
samFabrice <dsamfabrice5@gmail.com>
2021-11-03 11:08:09 +0100

    Final version of the readObjFile function allowing to load the obj file and to fill the vectors with the values ​​contained in the file

--- 3danimation.cpp
+++ 3danimation.cpp
@@ -5,7 +5,9 @@
 #include "Vect3D.hpp"
 
 using namespace std;
-void readObjFile();
+using namespace linalg;
+
+void readObjFile(vector<Vect3D> &vertexId, vector<Vect3D> &vertexTextureId, vector<Vect3D> &vertexNormalId, vector<int> &V, vector<int> &Vn, vector<int> &Vt);
 
 int main(){
     Matrice m1;
@@ -19,19 +21,25 @@ int main(){
     m1(2, 1) = 8;
     m1(2, 2) = 9;
   
-    
+    int i = 0;
     Vect3D vect( 3 , 1);
     Vect3D  multiplication { vect * m1 };
     cout << "Le produit de " << vect << " par "  << endl;
     cout << m1 << "donne " << endl;
     cout << multiplication << endl;
     cout << endl;
-    readObjFile();
+    vector<Vect3D> vertexId;
+    vector<Vect3D> vertexTextureId;
+    vector<Vect3D> vertexNormalId;
+    vector<int> V, Vt, Vn;
 
+    readObjFile(vertexId,  vertexTextureId, vertexNormalId, V, Vt, Vn);
+
+  
 }
 
-void readObjFile(){
-    FILE  *file = fopen("input.obj", "r");
+    void readObjFile(vector<Vect3D> &vertexId, vector<Vect3D> &vertexTextureId, vector<Vect3D> &vertexNormalId, vector<int> &V, vector<int> &Vn, vector<int> &Vt){
+    FILE  *file = fopen("input", "r");
     if (file == NULL){
         cout <<"Impossible de libre le fichier " << endl;
         exit(0);
@@ -42,23 +50,34 @@ void readObjFile(){
         if (result == EOF)
             break;
         if(strcmp(id , "v") == 0){
-            cout << "v " << " ->";
             Vect3D vect(3,1);
             fscanf(file, "%lf %lf %lf", &vect(0), &vect(1), &vect(2));
-            cout << vect;
+            vertexId.push_back(vect);
         }
-        else if(strcmp(id, "vt") == 0){
-            cout << "vt " << " ->";           
+        else if(strcmp(id, "vt") == 0){        
             Vect3D vect(2, 0);
             fscanf(file, "%lf %lf", &vect(0), &vect(1));
-            cout << vect;
+            vertexTextureId.push_back(vect);
         }
 
         else if(strcmp(id, "vn") == 0){
-            cout << "vn " << " ->";
             Vect3D vect(3, 0);
             fscanf(file, "%lf %lf %lf", &vect(0), &vect(1), &vect(2));
-            cout << vect;
+            vertexNormalId.push_back(vect);
+        }
+        else if(strcmp(id, "f") == 0){
+            int v[3], vn[3], vt[3];
+            fscanf(file, "%d/%d/%d %d/%d/%d %d/%d/%d", &v[0], &vt[0], &vn[0], &v[1], &vt[1], &vn[1], &v[2], &vt[2], &vn[2]);
+            //cout << v[0] << "/"<< vt[0] << "/" << vn[0] << "  " << v[1] << "/"<< vt[1] << "/" << vn[1] << "  " << v[2] << "/"<< vt[2] << "/" << vn[2] <<endl;
+            V.push_back(v[0]);
+            V.push_back(v[1]);
+            V.push_back(v[2]);
+            Vt.push_back(v[0]);
+            Vt.push_back(v[1]);
+            Vt.push_back(v[2]);
+            Vn.push_back(v[0]);
+            Vn.push_back(v[1]);
+            Vn.push_back(v[2]);
         }
     }
 }
